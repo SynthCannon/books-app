f422f7e442daeb28343e3aaf41a0b151d8028714
SynthCannon <niksum15@gmail.com>
2018-06-21 22:57:04 +0100

    working import.sql file without join

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,3 @@
-INSERT INTO BOOK (id, bookTitle, releaseYear, genre) VALUES (1, 'DatBoi Chronicles', '2018', 'Mystery'),(2, 'DatBoi Chronicles 2', '2022', 'Crime'),(3, 'DatBoi Chronicles 3','2025','Romance');
+INSERT INTO AUTHOR (AUTHOR_ID, name) VALUES (1,'DatBoi himself'),(2,'The Shafeeq'),(3,'The John');
+
+INSERT INTO BOOK (BOOK_ID, bookTitle, releaseYear, genre) VALUES (1, 'DatBoi Chronicles', '2018', 'Mystery'),(2, 'DatBoi Chronicles 2', '2022', 'Crime'),(3, 'DatBoi Chronicles 3','2025','Romance');
